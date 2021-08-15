51e604c2415a848fe1d48e44daae6c00244b4d9d
Kaarthik <52716626+kaarthik21@users.noreply.github.com>
2021-08-15 17:08:04 +0530

    Update cookie.php

--- container/scripts/cookie.php
+++ container/scripts/cookie.php
@@ -1,25 +1,36 @@
+
 <?php
-/**
- * Website: localhost/config.php
- **/
 
 if(!empty($_POST["remember"])) {
-	setcookie ("username",$_POST["username"],time()+ 3600);
-	setcookie ("password",$_POST["password"],time()+ 3600);
-	echo "Cookies Set Successfuly";
-	session_start(); $username = $password = $userError = $passError = '';
-	if(isset($_POST['sub'])){
-		$username = $_POST['username']; $password = $_POST['password'];
-			if($username === 'admin' && $password === 'superuserpass'){
-    				$_SESSION['login'] = true; header('LOCATION:moms.php'); die();
-			}
-		if($username !== 'admin')$userError = 'Invalid Username';
-		if($password !== 'password')$passError = 'Invalid Password';
-	}
-}else {
-	setcookie("username","");
-	setcookie("password","");
-	echo "Cookies Not Set";
+        setcookie ("username",$_POST["username"],time()+ 3600);
+        setcookie ("password",$_POST["password"],time()+ 3600);
+        echo "Cookies Set Successfuly";
+        session_start(); $username = $password = $userError = $passError = '';
+        if(isset($_POST['sub'])){
+                $username = $_POST['username']; $password = $_POST['password'];
+                if($username === 'admin' && $password === 'superuserpass'){
+                        $_SESSION['login'] = true; header('LOCATION:moms.php'); die();
+                        }
+                if($username !== 'admin')$userError = 'Invalid Username';
+                if($password !== 'password')$passError = 'Invalid Password';
+        }
+}
+elseif(empty($_POST["remember"])) {
+        setcookie ("username",$_POST["username"],time()+ 3600);
+        setcookie ("password",$_POST["password"],time()+ 3600);
+        echo "Cookies Not Set because remember me is not ticked!!";
+        session_start(); $username = $password = $userError = $passError= '';
+        if(isset($_POST['sub'])){
+                $username = $_POST['username']; $password = $_POST['password'];
+                if($username === 'admin' && $password === 'superuserpass'){
+                        $_SESSION['login'] = true; header('LOCATION:moms.php');die();
+                        }}
+}
+
+else {
+        setcookie("username","");
+        setcookie("password","");
+        echo "Cookies Not Set";
 }
 
 ?>
